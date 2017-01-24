8707e60354ea22444941c1a377379005d1e22507
Brian D. Hanna <brian.hanna@aarcorp.com>
2017-01-23 23:25:43 -0600

    expand and simplify room module

--- app/modules/room/room.js
+++ app/modules/room/room.js
@@ -1,13 +1,25 @@
+const uuid = require('uuid/v4');
 class Room {
-  constructor(socket, params) {
-    socket.role = 'owner';
+  constructor(user, params) {
+    this.id = uuid();
     this.name = params.room;
     this.code = params.code;
-    this.owner = {
-      id: socket.id,
-      name: socket.name
-    };
-    this.occupants = [];
+    this.owner = user.id;
+    this.members = {};
+  }
+
+  getRole(userId) {
+    let role;
+    if (userId === this.owner) {
+      role = 'owner';
+    } else if (Object.keys(this.members).indexOf(userId) > -1) {
+      role = 'member';
+    }
+    return role;
+  }
+
+  listMembers() {
+    return this.members;
   }
 }
 

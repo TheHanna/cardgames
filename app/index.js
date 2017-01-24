b73e88112c7bfad9c5acd083c4789764791962da
Brian D. Hanna <brian.hanna@aarcorp.com>
2017-01-23 23:24:29 -0600

    leverage new user module, rename events for clarity

--- app/index.js
+++ app/index.js
@@ -3,15 +3,8 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-// const games = {
-//   war: {
-//     players: {
-//       max: 2,
-//       min: 2
-//     }
-//   }
-// };
 const Rooms = new (require('./modules/room/rooms.js'));
+const Users = new (require('./modules/user/users.js'));
 
 app.use(express.static(`${__dirname}/../public`));
 
@@ -22,29 +15,26 @@ app.get('/', (req, res) => {
 // TODO: Abstract this out to separate file(s?)
 io.on('connection', (socket) => {
   debug(socket.id, 'connected');
-  // TODO: Abstract connection out to player class
   // TODO: Figure out reconnection
-  socket.on('init', (params) => {
-    debug(socket.id, 'named', params.name);
-    socket.name = params.name;
+  socket.on('user::create', (params) => {
+    Users.create(socket, params);
   });
-  socket.on('dude::bro', (params) => {
-    debug(params);
+  socket.on('room::create', (params) => {
+    Rooms.create(Users.find(params.user), params);
   });
-  socket.on('create', (params) => {
-    debug(socket.id, 'begin room creation', params.room);
-    Rooms.create(socket, params); // Create the room inside the room manager
+  socket.on('room::join', (params) => {
+    Rooms.join(Users.find(params.user), params.code);
   });
-  socket.on('join', (params) => {
-    Rooms.join(socket, params.code);
+  socket.on('room::leave', (params) => {
+    debug('room::leave triggered with params:', params);
+    Rooms.leave(Users.find(params.user), params.code);
   });
-  socket.on('leave', (code) => {
-    Rooms.leave(socket, code);
+  socket.on('disconnecting', () => {
+    socket.emit('user::disconnect');
+    // Users.destroy(Users.find(user));
   });
-  socket.on('shuffle', () => {
-    debug('shuffling');
-    // TODO: figure out how to allow only shuffling of the current game
-    // TODO: figure out how to allow only shuffling by the current dealer
+  socket.on('user::disconnect', (user) => {
+    Users.destroy(Users.find(user));
   });
   socket.on('disconnect', () => {
     debug(socket.id, 'disconnected!');

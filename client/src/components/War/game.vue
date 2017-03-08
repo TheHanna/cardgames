77f26646d619813429c0036db28d3dc3600da089
Brian D. Hanna <brian.hanna@aarcorp.com>
2017-03-08 01:01:47 -0600

    add styles, messages, events, etc

--- client/src/components/War/game.vue
+++ client/src/components/War/game.vue
@@ -6,11 +6,22 @@
       <button type="button" v-on:click="start" v-if="dealer">Start</button>
     </div>
     <div v-if="started">
-      <button type="button" v-on:click="play" v-if="started">Play</button>
+      <button type="button" v-on:click="play" v-if="started" :disabled="hasPlayed">Play</button>
     </div>
     <div class="errors">
       {{ errors[0] }}
     </div>
+    <div class="messages">
+      {{ messages[0] }}
+    </div>
+    <ul>
+      <li v-for="play in plays">
+        {{ play.player.name }}:
+        <span :class="play.card.suit">
+          {{ play.card.rank }}
+        </span>
+      </li>
+    </ul>
   </section>
 </template>
 
@@ -18,50 +29,85 @@
 export default {
   name: 'game',
   props: ['game', 'socket', 'room'],
-  data () {
+  data() {
     return {
       players: 0,
       code: this.room,
       dealer: false,
       started: false,
-      errors: []
+      errors: [],
+      messages: [],
+      plays: [],
+      hasPlayed: false
     }
   },
-  created () {
+  created() {
+    let c = strings => this.code + strings.join();
     if (!this.code) {
       this.socket.emit('game::create', this.game.name, game => {
         this.code = game.code;
         this.dealer = game.dealer;
         this.joined(game.players);
+        this.socket.on(c`::played`, this.played);
       });
     } else {
       this.socket.emit('game::join', this.code);
+      this.socket.on(c`::played`, this.played);
     }
     this.socket.on('game::joined', this.joined);
     this.socket.on('game::started', status => {
       this.started = true;
     });
+    this.socket.on(c`::message`, this.message);
     this.socket.on('game::error', this.error);
   },
   methods: {
-    start () {
+    start() {
       this.socket.emit('game::start', this.code);
     },
-    play () {
-      this.socket.emit(`${this.code}::start`, this.code);
+    play() {
+      this.socket.emit(`${this.code}::play`);
     },
-    joined (players) {
+    played(play) {
+      this.plays.push(play);
+      if (play.player.id === this.socket.id) this.hasPlayed = play.played;
+    },
+    joined(players) {
       this.players = players;
     },
-    error (message) {
+    error(message) {
       this.errors.unshift(message);
+    },
+    message(message) {
+      console.log(message);
+      this.messages.unshift(message);
     }
   }
 }
 </script>
 
 <style scoped>
-.errors {
-  color: red;
+.errors, .hearts, .diamonds {
+  color: #F00;
+}
+
+.clubs, .spades {
+  color: #000;
+}
+
+.hearts::after {
+  content: '\2665';
+}
+
+.diamonds::after {
+  content: '\2666';
+}
+
+.spades::after {
+  content: '\2660';
+}
+
+.clubs::after {
+  content: '\2663';
 }
 </style>

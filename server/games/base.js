a94f054ef0d8ba02edbe2857fa7fce3331742ae5
Brian D. Hanna <brian.hanna@aarcorp.com>
2017-03-08 01:00:41 -0600

    add server as default game property, remove hand as default player property

--- server/games/base.js
+++ server/games/base.js
@@ -4,11 +4,12 @@ const _ = require('lodash');
 class Game {
   constructor(server, code) {
     this.code = code;
+    this.server = server;
     this.room = server.sockets.adapter.rooms[code];
     this.players = {};
     _.keys(this.room.sockets).forEach(socket => {
       this.players[socket] = {
-        hand: []
+        client: server.sockets.sockets[socket]
       };
     });
   }

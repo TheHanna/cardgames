28ac3c843a0c1dde73e4703db4f8382ce126673d
TheHanna <hannaofdoom@gmail.com>
2017-03-10 17:14:44 -0600

    create menu component

--- client/src/components/Header/menu.vue
+++ client/src/components/Header/menu.vue
@@ -1,27 +1,30 @@
 <template lang="html">
   <nav class="pure-menu pure-menu-horizontal">
     <ul class="pure-menu-list">
+      <li class="pure-menu-item">
+        <router-link :to="{ name: 'Games', params: { user: user } }" class="pure-menu-link">
+          Games
+        </router-link>
+      </li>
       <li class="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
-        <create></create>
+        <a href="#" id="create" class="pure-menu-link">Create</a>
+        <create :user="user"></create>
       </li>
       <li class="pure-menu-item">
-        <button type="button" class="pure-button join" @click="$emit('join')">Join</button>
+        <button type="button" class="pure-button join">Join</button>
       </li>
     </ul>
   </nav>
 </template>
 
 <script>
+import create from '../Games/create';
+
 export default {
   name: 'menu',
-  props: ['user']
-  data() {
-    return {
-      games: ['war']
-    }
-  },
-  methods: {
-
+  props: ['user'],
+  components: {
+    create: create
   }
 }
 </script>

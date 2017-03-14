73b8de45c8eb7d1574a7f48be67767c88c5e7895
Brian D. Hanna <brian.hanna@aarcorp.com>
2017-03-14 00:15:19 -0500

    simplify games view, reference user from store instead of props

--- client/src/components/Games/index.vue
+++ client/src/components/Games/index.vue
@@ -1,68 +1,35 @@
 <template>
   <section>
     <h1>Games</h1>
-    <ul>
-      <li v-for="(room, code) in rooms" :class="{ open: !room.playing, closed: room.playing }">
-        {{ code }} ({{ room.game.name }}) - {{ room.length }}
-        <button type="button" @click="join(code)" v-if="!member(room)">Join</button>
-      </li>
-    </ul>
+    <game-list></game-list>
+    <component v-bind:is="game" v-if="game"></component>
   </section>
 </template>
 
 <script>
+import list from './list';
+
 export default {
   name: 'games',
-  props: ['user'],
+  components: {
+    'game-list': list
+  },
   data() {
-    return {
-      rooms: []
-    }
+    return { game: null }
   },
   created() {
-    console.log(this.user.socket);
-    if (!this.user.socket) {
-      this.$router.push('/');
-      return;
-    };
-    this.list();
-    this.user.socket.on('game::created', this.list);
-    this.user.socket.on('games::list', rooms => this.rooms = rooms);
+    if (!this.user.socket) this.$router.push('/');
   },
-  methods: {
-    list() {
-      this.user.socket.emit('games::list', rooms => { this.rooms = rooms; });
-    },
-    join(code) {
-      this.user.socket.emit('game::join', code);
-    },
-    member(room) {
-      return room.sockets[this.user.socket.id];
-    }
-
+  computed: {
+    user() { return this.$store.state.user }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 section {
   display: flex;
   height: 100%;
   flex-direction: column;
-}
-
-.open {
-  color: #0F0;
-}
-
-.closed {
-  color: #F00;
-}
-
-article { padding: 0 1em; }
-
-[type=submit]::after {
-  content: '\2714';
 }
 </style>
